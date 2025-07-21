45f882c1c29cf32796ada70f5eb3838f31229d46
mohammad rida <mhmd_rida_990@hotmail.com>
2025-07-21 18:02:54 +0300

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
-// import { verifyIdToken } from './lib/firebase-admin'; // optional if using Admin SDK
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
-
-  // List of protected routes
   const protectedPaths = ["/"];
 
   const isProtected = protectedPaths.some(
     (path) => req.nextUrl.pathname === path
   );
+  console.log(isProtected && !token);
 
   if (isProtected && !token) {
     const signInUrl = new URL("/auth/signin", req.url);
