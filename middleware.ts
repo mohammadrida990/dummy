dc292827c1d30970f3bf92875e03209c0cbb3261
mohammad rida <mhmd_rida_990@hotmail.com>
2025-07-21 20:49:13 +0300

    test

--- middleware.ts
+++ middleware.ts
@@ -17,5 +17,7 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)",
+  ],
 };
